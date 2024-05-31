a6266439b84d8eaf052b040a9048889be8958536
MaximeCasal <151937163+MaximeCasal@users.noreply.github.com>
2024-05-31 02:17:28 +0200

    gl

--- src/main/java/com/appjo/app_jo/Controlleur/AccueilController.java
+++ src/main/java/com/appjo/app_jo/Controlleur/AccueilController.java
@@ -5,16 +5,35 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class AccueilController {
 
-    @FXML
-    private void handleConnexion(Stage stage) throws IOException {
-
+    public void sportPage(MouseEvent mouseEvent) {
+        loadScene(mouseEvent, "/com/appjo/app_jo/Sport/Sport.fxml");
     }
 
-    public void handleConnexion(ActionEvent actionEvent) {
+    public void athletePage(MouseEvent mouseEvent) {
+        loadScene(mouseEvent, "/com/appjo/app_jo/AthletesScene.fxml");
+    }
+
+    public void calendrierPage(MouseEvent mouseEvent) {
+        loadScene(mouseEvent, "/com/appjo/app_jo/CalendarView.fxml");
+    }
+
+    private void loadScene(MouseEvent event, String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
